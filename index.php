e296d308e838c95a50e6f831baf172f397e6dd55
Tarcisio Silva <tarcisior19@hotmail.com>
2018-12-17 15:19:28 -0200

    Subindo projeto

---
+++ index.php
@@ -0,0 +1,186 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="utf-8" />
+		<meta http-equiv="X-UA-Compatible" content="IE=edge">
+		<title>Page Title</title>
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<!--	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous"> -->
+		<link href="https://fonts.googleapis.com/css?family=Poppins" rel="stylesheet">
+		<link href="https://fonts.googleapis.com/css?family=Fjalla+One|IBM+Plex+Mono" rel="stylesheet">
+		<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.5.0/css/all.css" integrity="sha384-B4dIYHKNBt8Bc12p+WXckhzcICo0wtJAoU8YZTY5qE0Id1GSseTk6S+L3BlXeVIU" crossorigin="anonymous">
+		<link rel="stylesheet" href="assets/css/bootstrap.min.css">
+		<link rel="stylesheet" type="text/css" media="screen" href="assets/css/main.css" />
+		
+	</head>
+	<body>
+		
+		<header id="home">
+				<div class="header-menu">
+						<div class="menu-toggle">
+							<span></span>
+							<span></span>
+							<span></span>
+						</div>	
+						<nav>
+							<ul>
+								<li><a href="#">Home</a></li>
+								<li><a href="#">About</a></li>
+								<li><a href="#">Services</a></li>
+								<li><a href="#">Portfolio</a></li>
+								<li><a href="#">Contact</a></li>
+							</ul>
+						</nav>
+				</div>
+				<div class="texts">
+						<!-- <img src="assets/image/robo-img.png"> -->
+					<img src="assets/image/logo.png"> 
+					<h1>Flag<span>Developer</span></h1>
+				<!--	<h1>Tarcisio Silva</h1> -->
+					<p>Desenvolvimento Web / Mobile</p>
+					<div class="arrow">
+						<a href="#">
+							<i class="fas fa-arrow-down"></i>
+						</a>
+					</div>
+				</div>
+		</header>
+		<section id="about">
+		<div class="container">	
+			<div class="row">
+				<div class="col-sm">
+				<h1 class="title">Sobre</h1>
+				</div>
+			</div>
+				<div class="row">
+					<div class="col-sm-4">
+						<div class="about-logo">
+							<i class="fas fa-code"></i>
+						</div>
+					</div>
+					<div class="col-sm">
+						<div class="about-text">
+							<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Iste delectus magnam libero laborum fuga reiciendis recusandae, quam tempora autem repellendus rem aperiam nihil nulla ex? Inventore blanditiis necessitatibus provident quae!</p>
+							<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Iste delectus magnam libero laborum fuga reiciendis recusandae, quam tempora autem repellendus rem aperiam nihil nulla ex? Inventore blanditiis necessitatibus provident quae!</p>
+						</div>
+					</div>
+				</div>
+
+				<div class="row">
+					<div class="col-sm">
+						<div class="about-text">
+							<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Iste delectus magnam libero laborum fuga reiciendis recusandae, quam tempora autem repellendus rem aperiam nihil nulla ex? Inventore blanditiis necessitatibus provident quae!</p>
+							<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Iste delectus magnam libero laborum fuga reiciendis recusandae, quam tempora autem repellendus rem aperiam nihil nulla ex? Inventore blanditiis necessitatibus provident quae!</p>
+						</div>
+					</div>
+					<div class="col-sm-4">
+						<div class="about-logo">
+							<i class="fas fa-code"></i>
+						</div>
+					</div>
+				</div>
+			</div>
+		</section>
+
+		<section id="service">
+		<div class="container">
+		  <div class="row">
+				<div class="col-sm">
+					<h1 class="title">Serviços</h1>
+				</div>
+			</div>
+			<div class="row">
+					<div class="col-sm marTop20">
+						<p>
+						Lorem Ipsum é simplesmente uma simulação de texto da indústria tipográfica e de impressos, e vem sendo utilizado desde o século XVI, quando um impressor desconhecido pegou uma bandeja de tipos e os embaralhou para fazer um livro de modelos de tipos. Lorem Ipsum sobreviveu não só a cinco séculos, como também ao salto para a editoração eletrônica.
+						</p>
+					</div>
+			</div>
+			<div class="row marTop20">
+				<div class="col-sm">Section 1</div>
+				<div class="col-sm">Section 2</div>
+				<div class="col-sm">Section 3</div>
+			</div>
+    </div>
+		</section>
+
+		<section id="portfolio">
+			<div class="container">
+				<div class="row">
+					<div class="col-sm">
+						<h1 class="title">Portfólio</h1>
+					</div>
+				</div>
+				<div class="row">
+						<div class="col-sm marTop20">
+							<p>
+							Lorem Ipsum é simplesmente uma simulação de texto da indústria tipográfica e de impressos, e vem sendo utilizado desde o século XVI, quando um impressor desconhecido pegou uma bandeja de tipos e os embaralhou para fazer um livro de modelos de tipos. Lorem Ipsum sobreviveu não só a cinco séculos, como também ao salto para a editoração eletrônica.
+							</p>
+						</div>
+				</div>
+				<div class="row">
+					<div class="col-sm-3">Box</div>
+					<div class="col-sm-3">Box</div>
+					<div class="col-sm-3">Box</div>
+					<div class="col-sm-3">Box</div>
+					<div class="col-sm-3">Box</div>
+					<div class="col-sm-3">Box</div>
+					<div class="col-sm-3">Box</div>
+					<div class="col-sm-3">Box</div>
+				</div>
+				<div class="row">
+					<a href="#">Ver mais</a>
+				</div>
+			</div>
+		</section>
+
+		<section id="contact">
+			<div class="container">
+					<div class="row">
+						<div class="col-sm">
+							<h1 class="title">Contato</h1>
+						</div>
+					</div>
+					<div class="row">
+							<div class="col-sm marTop20">
+								<p>
+								Lorem Ipsum é simplesmente uma simulação de texto da indústria tipográfica e de impressos, e vem sendo utilizado desde o século XVI, quando um impressor desconhecido pegou uma bandeja de tipos e os embaralhou para fazer um livro de modelos de tipos. Lorem Ipsum sobreviveu não só a cinco séculos, como também ao salto para a editoração eletrônica.
+								</p>
+							</div>
+					</div>
+					<div class="row marTop20">
+						<div class="col-sm">
+							<h3>Envie sua menssagem!</h3>	
+							<form>
+								<input type="text" placeholder="Digite o nome" name="nome"><br/>
+								<input type="email" placeholder="Digite o e-mail" name="email"><br/>
+								<textarea>Digite a Menssagem</textarea><br/>
+								<input type="submit" value="Enviar">
+							</form>
+						</div>
+						<div class="col-sm">
+							<h3>Ou escolha uma forma de contato</h3>
+							<div class="tabe-contact">
+								<span class="tab-left">E-mail</span>
+								<span class="tab-right">tarcisio101093@gmail.com</span>
+							</div>
+						</div>
+					</div>
+			</div>
+		</section>
+
+		<footer>
+			<div class="container">
+				<div class="row">
+					<div class="col-sm">
+						<span>Todos os diretos reservados a <a href="#">FlagDev</a>!</span>
+					</div>
+				</div>
+			</div>
+		</footer>
+		<script src="assets/js/jquery.min.js"></script>
+		<script src="assets/js/bootstrap.bundle.min.js"></script>
+		<script src="assets/js/bootstrap.min.js"></script>
+		<script src="assets/js/javascript.js"></script>
+	</body>
+</html>
